30c2943316bb403feca22a88bb00df2ec4f5f9b3
HarischandraGDS <89300910+HarischandraGDS@users.noreply.github.com>
2023-05-17 18:21:05 -0700

    design collection plastic details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,16 +32,49 @@ retrievePlastic(){
 
   render() {
     return (
-      <div>
-        {this.state.plastics.map(sellers =>(
-         <div>
-          <p>{sellers.name}</p>
-          <p>{sellers.mobile}</p>
-          <p>{sellers.weight}</p>
-          <p>{sellers.price}</p>
-         </div>
+      <div className="container">
+        &nbsp;
+        &nbsp;
+        <center><p><h3>Collection Plastic Details</h3></p></center>
+        &nbsp;
+        &nbsp;
+        <table class="table">
+          <thead>
+           <tr>
+            <th scope='col'></th>
+            <th scope='col'>NAME</th>
+            <th scope='col'>MOBILE</th>
+            <th scope='col'>PLASTIC WEIGHT (kg)</th>
+            <th scope='col'>PRICE TO PLASTICS (Rs)</th>
+            <th scope='col'></th>
+
+           </tr>
+
+          </thead>
+          <tbody>
+        {this.state.plastics.map((sellers,index) =>(
+         <tr>
+          <th scope="row">{index+1}</th>
+          <td>{sellers.name}</td>
+          <td>{sellers.mobile}</td>
+          <td>{sellers.weight}</td>
+          <td>{sellers.price}</td>
+          <td>
+
+            <a className="btn btn-warning" href='#'>
+              <i className="fas fa-edit"></i>&nbsp;EDIT
+            </a>
+            &nbsp;
+            <a className="btn btn-danger" href='#'>
+              <i className="far fa-trash-alt"></i>&nbsp;DELETE
+            </a>
+
+          </td>
+         </tr>
 
         ))}
+        </tbody>
+        </table>
       </div>
     )
   }
