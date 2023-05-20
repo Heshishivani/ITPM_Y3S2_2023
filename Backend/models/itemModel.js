05e3de5c73d8e7753fc470adeb505489abf53beb
Heshini Weerasinghe <heshimongodb@gmail.com>
2023-05-20 08:00:59 +0530

    User model updated

---
+++ Backend/models/itemModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const itemModel = mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    available_sizes: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+    material: {
+      type: String,
+      required: false,
+    },
+    images: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Item = mongoose.model("items", itemModel);
+export default Item;
